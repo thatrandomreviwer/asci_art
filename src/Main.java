32eac4be0982f627fb5497e5e74720cd11480186
100133917 <100133917@lubbockisd.net>
2022-09-01 08:54:03 -0500

    changed name to reflect art

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void brick(String[] args) {
         System.out.println("_____________");
         System.out.println("|     .   . |");
         System.out.println("|     .     |");
